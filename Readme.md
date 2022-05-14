9697f1f412b25aa000c02bac30169f848c4708fe
songyaoshun <songyaoshun@gmail.com>
2022-05-14 10:46:37 +0800

    支持自定义区块高度

---
+++ Readme.md
@@ -0,0 +1,2 @@
+## commands
+- `python blockTxns.py -bs 1000552 -be 1000581 -g 100`
--- blockTxns.py
+++ blockTxns.py
@@ -7,21 +7,34 @@ from web3.middleware import geth_poa_middleware
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 
-
-def blockTxns():
+def blockTxns(bs: int, be: int, gap: int = 100, xStep: int = 10):
 
     # 链接 rpc
     teleportClient = Web3(Web3.HTTPProvider('https://teleport-localvalidator.qa.davionlabs.com/'))
     teleportClient.middleware_onion.inject(geth_poa_middleware, layer=0)
-    last = teleportClient.eth.blockNumber
+    # block-txns 的二维数组
     data = [[],[]]
-    for i in range(last-100, last):
+    # 汇总 txns
+    count = 0
+
+    if be == 0:
+        be = teleportClient.eth.blockNumber
+    if bs == 0 or gap != 100:
+        bs = be - gap
+
+    for i in range(bs, be):
         block = teleportClient.eth.getBlock(i)
         txns = len(block.transactions)
+        count += txns
+
         pprint([block.number,block.timestamp,txns])
+
         data[0].append(i)
         data[1].append(txns)
+
+    print(f"上链的txns总数为: {count}")
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -32,12 +45,19 @@ def blockTxns():
     ax.set_xlabel('blockNums')
     ax.set_ylabel('Txns', fontdict={"family": "Times New Roman", "size": 25})
 
-    ax.set_xticks(np.arange(data[0][0],data[0][-1]+10,10))
+    ax.set_xticks(np.arange(data[0][0], data[0][-1] + xStep, xStep))
     fig.autofmt_xdate()
 
     plt.show()
 
 
 if __name__ == "__main__":
-    blockTxns()
+    parser = argparse.ArgumentParser()
+    parser.description='支持定义blockNum,或者获取最新区块'
+    parser.add_argument("-bs", "--blockStart", help="起始区块", dest="bs", type=int, default=0)
+    parser.add_argument("-be", "--blockEnd", help="截止区块", dest="be", type=int, default=0)
+    parser.add_argument("-g", "--gap", help="区块间隔,用于自动获取起始区块", dest="g", type=int, default=100)
+    args = parser.parse_args()
+
+    blockTxns(args.bs,args.be,args.g)
 
